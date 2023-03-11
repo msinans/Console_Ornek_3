3b49dcb3253eb06d23f05d5d3cad3bb5f1e9f0e3
m.sinans <msomercikoglu@gmail.com>
2023-03-11 14:37:53 +0300

    11.03.2023

--- Console_Ornek_3/Program.cs
+++ Console_Ornek_3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Console_Ornek_3
 {
@@ -15,6 +11,7 @@ namespace Console_Ornek_3
                ks.kisilistesi("Ali Alper");
                Console.Read(); */
 
+            // Klavyeden girileceki isim on kere yazdırılır
             Kisiler ks = new Kisiler();
             string ads;
             Console.Write("Bİr isim giriniz!: ");
