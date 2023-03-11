90bd0ddfe4a03b7e132710d0bc0a3beea3c01a16
m.sinans <msomercikoglu@gmail.com>
2023-03-11 11:25:10 +0300

    Add project files.

---
+++ Console_Ornek_3/Kisiler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ornek_3
+{
+    internal class Kisiler
+    {
+        public void kisilistesi(string adsoyad)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(adsoyad);
+            }
+
+        }
+
+    }
+}
---
+++ Console_Ornek_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ornek_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Aşağıdaki isimler on kere yazdırılır
+            /* Kisiler ks = new Kisiler();
+               ks.kisilistesi("Murat Sadi");
+               ks.kisilistesi("Ali Alper");
+               Console.Read(); */
+
+            Kisiler ks = new Kisiler();
+            string ads;
+            Console.Write("Bİr isim giriniz!: ");
+            ads = Console.ReadLine();
+            ks.kisilistesi(ads);
+            Console.Read();
+        }
+    }
+}
